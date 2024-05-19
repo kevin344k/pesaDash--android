0dd3a2173f6d002219b12e587576aa3f1136d053
kevin344k <kevin344k@gmail.com>
2024-05-18 23:48:12 -0500

    primeros pasos en la app dash

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#04103A',
     alignItems: 'center',
     justifyContent: 'center',
   },
